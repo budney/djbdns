e1b01cec2911ec3b3a10e99f6a5f7b2ed882a64c
Len Budney <Len.Budney@grantstreet.com>
2020-08-22 08:54:59 -0400

    Added support for a forward-only cache and for delegating specified domains to tinydns from the cache.

--- start.sh
+++ start.sh
@@ -12,9 +12,26 @@ case "$SERVICE" in
     dnscache)
         echo "Running dnscache."
 
-        # Authorize the default gateway to talk to us
-        cd "/srv/$SERVICE/root/ip" && \
-        touch `route -n | grep 'UG[ \t]' | awk '{print $2}' | cut -f1 -d.`
+        # Options 1 & 2 are identical in this setup: a
+        # cache that accepts queries over the network.
+        NET=$( netstat -rn | egrep UG | awk '{print $2}' | cut -f1 -d. )
+        touch "/srv/$SERVICE/root/ip/$NET"
+
+        # Options 3 & 4 are identical in this setup: a
+        # forward-only cache that accepts queries over
+        # the network.
+        if [ -n "$forwardto" ]; then
+            echo 1 > "/srv/$SERVICE/env/FORWARDONLY"
+            echo "$forwardto" > "/srv/$SERVICE/root/servers/@"
+        fi
+
+        # Delegate the specified domains, if any, to
+        # tinydns for resolution.
+        if [ -n "$delegate" ]; then
+            for DOMAIN in $( echo "$delegate" | tr : ' ' ); do
+                dnsip tinydns > "/srv/$SERVICE/root/servers/$DOMAIN"
+            done
+        fi
         ;;
 
     *)
